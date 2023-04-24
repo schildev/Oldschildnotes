7299cea7b08263ffe45abcbc828e3d6bdbae69fb
Schildkrote-Communist <108930911+Schildkrote-Communist@users.noreply.github.com>
2023-04-24 21:01:29 +0200

    Update db.ts

--- db/db.ts
+++ db/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 async function dbConnect() {
-  return mongoose.connect(process.env.DB as string, {bufferCommands:false});
+  return mongoose.connect("mongodb+srv://schildNotes:ejfaojfoKODKOAZKDZapdjpaok@miliosa.r6ftxgq.mongodb.net/Schildnotes?retryWrites=true&w=majority");
 }
 
 export default dbConnect
