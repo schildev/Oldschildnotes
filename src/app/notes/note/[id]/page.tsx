c4a6e12fdc3f5179c16440b324cc40a5222dfb4f
schildscans <schildscans@gmail.com>
2023-05-22 22:29:29 +0200

    fix issue

--- src/app/notes/note/[id]/page.tsx
+++ src/app/notes/note/[id]/page.tsx
@@ -33,7 +33,7 @@ const Note = async ({params}:any) => {
     const markdownToHtml = await getHtml(noteToShow.content);
 
     return (
-        <div className={"flex items-center min-h-[75dvh]"} style={{marginInlineStart:"35px!important", listStyleType:"circle!important"}}>
+        <div className={"flex items-center min-h-[75dvh]"}>
             <NoteContent html={markdownToHtml} noteToShow={noteToShow} MuktaFont={MuktaFont} HeadingRubikFont={RubikFont} />
         </div>
     );
