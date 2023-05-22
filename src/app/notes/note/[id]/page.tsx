36cb013e92b0a02c02fcc3f5c6ccd87ebba21b3a
schildscans <schildscans@gmail.com>
2023-05-22 22:24:25 +0200

    commit list style

--- src/app/notes/note/[id]/page.tsx
+++ src/app/notes/note/[id]/page.tsx
@@ -33,7 +33,7 @@ const Note = async ({params}:any) => {
     const markdownToHtml = await getHtml(noteToShow.content);
 
     return (
-        <div className={"flex items-center min-h-[75dvh]"}>
+        <div className={"flex items-center min-h-[75dvh]"} style={{marginInlineStart:"35px", listStyleType:"circle"}}>
             <NoteContent html={markdownToHtml} noteToShow={noteToShow} MuktaFont={MuktaFont} HeadingRubikFont={RubikFont} />
         </div>
     );
