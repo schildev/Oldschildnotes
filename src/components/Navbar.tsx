8cc36c2290c6944484f51164271300ef94827bd8
schildev <killersada12@hotmail.com>
2023-09-03 21:35:14 +0200

    download app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <header className={"bg-indigo-500 border-b shadow-md shadow-violet-300/50 py-3 align-center justify-start gap-3 border-violet-600"}>
                 <div className={"mx-2 flex-wrap  md:flex-nowrap items-center flex justify-between  md:justify-end gap-3 text-sky-100"}>
-                <h2 className={"flex basis-8/12 md:basis-4/12 lg:basis-3/12 justify-start md:justify-center items-center text-3xl text-start " + HeadingRowdiesFont.className}>Schildnotes</h2>
+                <h2 className={"flex basis-8/12 md:basis-4/12 lg:basis-3/12 justify-start md:justify-center items-center text-3xl text-start " + HeadingRowdiesFont.className}><Link href={"/"}>Schildnotes</Link></h2>
                     <SearchForm />
                     <NavbarLinks />
                 </div>
--- src/components/NavbarLinks.tsx
+++ src/components/NavbarLinks.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 const NavbarLinks = () => {
     return (
         <div className={"flex gap-3 md:gap-5 order-1 items-center md:order-2 m-0 md:basis-2/12 lg:basis-1/12 justify-end md:justify-center"}>
-    <Link className={"text-lg grow-0 text-white flex items-center"} href={"/"}>
-        <span className={"material-symbols-outlined text-2xl"}>
-            cottage
-        </span>
-    </Link>
     <Link className={"text-lg grow-0 text-white flex items-center"} href={"/notes/create"}>
     <span className="material-symbols-outlined text-2xl">
         note_add
     </span>
+    </Link>
+    <Link className={"text-lg grow-0 text-white flex items-center"} href={"/schildnotes.apk"}>
+        <span className={"material-symbols-outlined text-2xl"}>
+            app_promo
+        </span>
     </Link>
 </div>
     );
